234c9ac8003cc435e7446cc5266c8ae7b3cae197
Magudarena <149296348+Magudarena@users.noreply.github.com>
2026-01-09 20:40:13 +0100

    Implement a simple HTTP server with a greeting

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Witaj w projekcie DevOps!")
+def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Serwer startuje na porcie {port}")
+    httpd.serve_forever()
+if __name__ == "__main__":
+    run()
