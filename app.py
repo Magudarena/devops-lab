63286a857fbebbcdea4f8904248c90536a7a2c1e
Magda <bfe2f9@gmail.com>
2026-01-10 03:05:07 +0100

    final

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+
 class SimpleHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"Witaj w projekcie DevOps!")
+        self.wfile.write(b"Witaj! To jest rozbudowana wersja projektu po Merge Request.")
+
 def run(server_class=HTTPServer, handler_class=SimpleHandler, port=8000):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print(f"Serwer startuje na porcie {port}")
+    print(f"Serwer dziala na porcie {port}...")
     httpd.serve_forever()
+
 if __name__ == "__main__":
     run()
 
